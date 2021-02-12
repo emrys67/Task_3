cb89e295ef90353661688197af131da1e831fd5b
emrys <emriesy@gmail.com>
2021-02-13 00:36:10 +0100

    Task Redone

---
+++ src/AnagramTwo.java
@@ -0,0 +1,29 @@
+public class AnagramTwo {
+    public String getAnagram(String input){
+        String output = null;
+        String inputArray[] = input.split(" ");
+        String outputArray[] = new String[inputArray.length];
+        for(int a = 0; a < inputArray.length;a++) {
+            int count = 0;
+            char chars[] = new char[inputArray[a].length()];
+            char chars_out[] = new char[inputArray[a].length()];
+            for (int i = 0; i < inputArray[a].length(); i++) {
+                if (Character.isAlphabetic(inputArray[a].charAt(i))) {
+                    chars[count] = inputArray[a].charAt(i);
+                    count++;
+                }
+            }
+            count--;
+            for (int i = 0; i < inputArray[a].length(); i++) {
+                if (Character.isAlphabetic(inputArray[a].charAt(i))) {
+                    chars_out[i] = chars[count];
+                    count--;
+                } else
+                    chars_out[i] = inputArray[a].charAt(i);
+            }
+            outputArray[a] = new String(chars_out);
+        }
+        output = String.join(" ", outputArray);
+        return output;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        AnagramTwo a = new AnagramTwo();
+        AnagramOne b = new AnagramOne();
+        System.out.println(a.getAnagram("App2lic1ation support!ing th!e for3ei4gn adminis12tration of a current2ly selected stu321dies."));
+        System.out.println(b.getAnagram());
+    }
+}
